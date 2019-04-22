44f161ec72d0939c51e71a42df187dd356aa497f
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2019-04-22 15:26:02 -0400

    Add some headers for EFI_RUNTIME_SERVICES

--- include/uefi/RuntimeServices.h
+++ include/uefi/RuntimeServices.h
@@ -6,13 +6,30 @@
 #ifndef UEFI_RUNTIME_SERVICES_H
 #define UEFI_RUNTIME_SERVICES_H
 
+// EFI_TABLE_HEADER(Page 163)
+
+typedef struct {
+	UINT64 Signature;
+	UINT32 Revision;
+	UINT32 HeaderSize;
+	UINT32 CRC32;
+	UINT32 Reserved;
+} EFI_TABLE_HEADER;
+
 // GetTime?
 
-// SetTime?
+// EFI_SET_TIME(Page 327)
+// EFI_TIME?
+typedef EFI_STATUS SetTime(EFI_TIME *Time);
 
-// GetWakeupTime?
+// EFI_GET_WAKEUP_TIME(Page 328)
 
-// SetWakeupTime?
+typedef EFI_STATUS GetWakeupTime(typedef EFI_STATUS GetWakeupTime(BOOLEAN *Enabled, BOOLEAN *Pending, EFI_TIME *Time);
+
+// EFI_SET_WAKEUP_TIME(Page 329)
+
+typedef EFI_STATUS SetWakeupTime(BOOLEAN Enable, EFI_TIME *Time);
+typedef EFI_STATUS SetWakeupTime(BOOLEAN Enable);
 
 // SetVirtualAddressMap?
 
