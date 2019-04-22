de4861324225ef67849386479e092667be14829b
Sakib Pathen <37029585+todalta@users.noreply.github.com>
2019-04-21 22:25:59 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <uefi.h>
 
 // TODO: efi_main
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable0 {
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   //main goes here
 }
