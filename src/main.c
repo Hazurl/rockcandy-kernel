444ca7094a8d86fba61546437dbae7415d75b74f
Alexander Hill <CodeBreadpudding@gmail.com>
2019-04-22 21:11:52 -0400

    Tabs > Spaces

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <uefi.h>
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
 	ConOut->OutputString(ConOut, U"Welcome to Rockcandy!");
 }
