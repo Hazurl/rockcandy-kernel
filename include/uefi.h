904af601c787eb0ba074a6affa40877452cb18a2
Alexander Hill <CodeBreadpudding@gmail.com>
2019-04-22 20:19:01 -0400

    Sorted uefi.h alphabetically

--- include/uefi.h
+++ include/uefi.h
@@ -1,7 +1,7 @@
-#include <uefi/types.h>
-#include <uefi/SystemTable.h>
+#include <uefi/BootServices.h>
 #include <uefi/EntryPoint.h>
+#include <uefi/RuntimeServices.h>
 #include <uefi/SimpleTextInput.h>
 #include <uefi/SimpleTextOutput.h>
-#include <uefi/BootServices.h>
-#include <uefi/RuntimeServices.h>
+#include <uefi/SystemTable.h>
+#include <uefi/types.h>
