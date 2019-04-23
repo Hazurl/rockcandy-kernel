17994442460045ba12c6f629cedb46b90dbadb92
Alexander Hill <CodeBreadpudding@gmail.com>
2019-04-23 14:39:52 -0400

    Fixed the Unicode string

--- src/main.c
+++ src/main.c
@@ -2,5 +2,5 @@
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
-	ConOut->OutputString(ConOut, U"Welcome to Rockcandy!");
+	ConOut->OutputString(ConOut, u"Welcome to Rockcandy!");
 }
