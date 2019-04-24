3cd12573c1fa45e7de1469e8898eb8decd532dc9
Alexander Hill <CodeBreadpudding@gmail.com>
2019-04-23 22:03:20 -0400

    Added EFI_STATUS values

--- src/main.c
+++ src/main.c
@@ -3,4 +3,5 @@
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
 	ConOut->OutputString(ConOut, u"Welcome to Rockcandy!");
+	return EFI_SUCCESS;
 }
