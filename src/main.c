24eb7460f6e989b3faed336b27815eb1bdabd08b
Alexander Hill <CodeBreadpudding@gmail.com>
2019-05-02 19:59:50 -0400

    Removed the test code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 #include <uefi.h>
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
-	ConOut->OutputString(ConOut, u"Welcome to Rockcandy!\r\n");
-	return EFI_SUCCESS;
+	//
 }
