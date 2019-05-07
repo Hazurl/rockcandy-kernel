0232d053f82d08cbd45b640f73131f66f0b05b58
Hazurl <ricoujules69@gmail.com>
2019-05-08 01:12:48 +0200

    Query File Protocol and fix GUID (#14)

--- src/main.c
+++ src/main.c
@@ -11,14 +11,27 @@ void kprint(CHAR16 *str) {
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	(void)ImageHandle;
-	VOID *fs;
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* file_system = 0;
+	EFI_FILE_PROTOCOL* file_protocol = 0;
+	EFI_GUID file_system_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+
 	ConOut = SystemTable->ConOut;
 	ConOut->ClearScreen(ConOut);
 	ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(13, 0));
 	kprint(u"Welcome to Rockcandy!\r\n");
 	ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(15, 0));
-	if(SystemTable->BootServices->LocateProtocol((EFI_GUID*)&EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID, 0, &fs) == EFI_SUCCESS) {
-		//
+	
+	if(SystemTable->BootServices->LocateProtocol(&file_system_guid, 0, (void**)&file_system) == EFI_SUCCESS) {
+		ConOut->OutputString(ConOut, u"Got EFI_SIMPLE_FILE_SYSTEM_PROTOCOL!\r\n");
+		if (file_system->OpenVolume(file_system, &file_protocol) == EFI_SUCCESS) {
+			ConOut->OutputString(ConOut, u"Got EFI_FILE_PROTOCOL!\r\n");
+		} else {
+			ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(12, 0));
+			ConOut->OutputString(ConOut, u"An unexpected error occured\r\n");
+			ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(15, 0));
+		}
+
 	} else {
 		panic();
 	}
