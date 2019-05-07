5047a45e2b9109e10db179d50cf2114b6c639ceb
Alexander Hill <CodeBreadpudding@gmail.com>
2019-05-07 12:08:32 -0400

    Added an infinite loop right before the return in efi_main

--- src/main.c
+++ src/main.c
@@ -3,16 +3,18 @@
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut = SystemTable->ConOut;
+	VOID *fs;
 	ConOut->ClearScreen(ConOut);
 	ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(13, 0));
 	ConOut->OutputString(ConOut, u"Welcome to Rockcandy!\r\n");
 	ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(15, 0));
-	if(SystemTable->BootServices->LocateProtocol(EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID) == EFI_SUCCESS) {
+	if(SystemTable->BootServices->LocateProtocol(&EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID, 0, &fs) == EFI_SUCCESS) {
 		//
 	} else {
 		ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(12, 0));
 		ConOut->OutputString(ConOut, u"An unexpected error occured\r\n");
 		ConOut->SetAttribute(ConOut, EFI_TEXT_ATTR(15, 0));
 	}
+	while(1) {}
 	return EFI_SUCCESS;
 }
